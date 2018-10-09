3f8e44465bc2efe74ab2ff8df28e7ca56f053f83
ashleyparrilla <43651597+ashleyparrilla@users.noreply.github.com>
2018-10-09 11:26:33 -0700

    10/9/18 scripts

---
+++ python_use_module.py
@@ -0,0 +1,3 @@
+import test_module as tm  #all of the functions from this script 
+                          #will be accessible through tm 
+tm.hello_world()
---
+++ test_module.py
@@ -0,0 +1,2 @@
+def hello_world():    #defining our function
+	print("Hello world, we made a module!")
